2227d736c629f171b1df5039c18f7d85380198fd
Ihor Trypolskyi <itrypolskyi@hubspot.com>
2023-12-04 09:11:13 +0100

    Next days empty classes

---
+++ src/aoc2015/Aoc1508.java
@@ -0,0 +1,15 @@
+package aoc2015;
+
+public class Aoc1508 {
+  static private final boolean TEST = true;
+
+  public static void main(String[] args) {
+    System.out.println(4);
+  }
+
+  private static String[] getInput() {
+    String testStr = "";
+    String realStr = "";
+    return (TEST ? testStr : realStr).split("\n");
+  }
+}
---
+++ src/aoc2023/Aoc2305.java
@@ -0,0 +1,15 @@
+package aoc2023;
+
+public class Aoc2305 {
+  static private final boolean TEST = true;
+
+  public static void main(String[] args) {
+    System.out.println(4);
+  }
+
+  private static String[] getInput() {
+    String testStr = "";
+    String realStr = "";
+    return (TEST ? testStr : realStr).split("\n");
+  }
+}
