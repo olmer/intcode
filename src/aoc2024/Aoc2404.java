137d8e9bfc8a10288ad98695c47ac0b43aa32e2c
Ihor Trypolskyi <itrypolskyi@hubspot.com>
2024-12-04 11:52:37 +0000

    2404

--- src/aoc2024/Aoc2404.java
+++ src/aoc2024/Aoc2404.java
@@ -1,12 +1,64 @@
 package aoc2024;
 
+import java.util.List;
+import java.util.Map;
+
+import tools.Grid;
+import tools.Grid.Direction;
+import tools.Pair;
+
 public class Aoc2404 extends AbstractAoc {
   long part1(String[] in) {
-    return 0;
+    int r = 0;
+    for (int i = 0; i < in.length; i++) {
+      for (int j = 0; j < in[i].length(); j++) {
+        if (in[i].charAt(j) != 'X') {
+          continue;
+        }
+        Map<Direction, List<Pair<Character, Pair<Integer, Integer>>>> dirs = Grid.getBeams(j, i, in, 3, Direction.values());
+        for (List<Pair<Character, Pair<Integer, Integer>>> beam : dirs.values()) {
+          List<Character> chars = beam.stream().map(Pair::getKey).toList();
+          if (chars.equals(List.of('M', 'A', 'S'))) {
+            r++;
+          }
+        }
+      }
+    }
+
+    return r;
   }
 
   long part2(String[] in) {
-    return 0;
+    int r = 0;
+    for (int i = 0; i < in.length; i++) {
+      for (int j = 0; j < in[i].length(); j++) {
+        if (in[i].charAt(j) != 'A') {
+          continue;
+        }
+        Map<Direction, List<Pair<Character, Pair<Integer, Integer>>>> dirs = Grid.getBeams(j, i, in, 1, Direction.NE, Direction.SE, Direction.SW, Direction.NW);
+        int validDiagonals = 0;
+        if (dirs.get(Direction.NE) == null || dirs.get(Direction.NE).isEmpty() || dirs.get(Direction.SW) == null || dirs.get(Direction.SW).isEmpty()
+            || dirs.get(Direction.NW) == null || dirs.get(Direction.NW).isEmpty() || dirs.get(Direction.SE) == null || dirs.get(Direction.SE).isEmpty()
+        ) {
+          continue;
+        }
+        if (dirs.get(Direction.NE).get(0).getKey() == 'M' && dirs.get(Direction.SW).get(0).getKey() == 'S'
+            || dirs.get(Direction.NE).get(0).getKey() == 'S' && dirs.get(Direction.SW).get(0).getKey() == 'M'
+        ) {
+          validDiagonals++;
+        }
+        if (dirs.get(Direction.NW).get(0).getKey() == 'M' && dirs.get(Direction.SE).get(0).getKey() == 'S'
+            || dirs.get(Direction.NW).get(0).getKey() == 'S' && dirs.get(Direction.SE).get(0).getKey() == 'M'
+        ) {
+          validDiagonals++;
+        }
+        if (validDiagonals == 2) {
+          r++;
+        }
+      }
+    }
+
+    return r;
   }
 
   public static void main(String[] args) {
@@ -15,21 +67,39 @@ public class Aoc2404 extends AbstractAoc {
 
   @Override
   long getTestExpected1() {
-    return 0;
+    return 18;
   }
 
   @Override
   long getTestExpected2() {
-    return 0;
+    return 9;
   }
 
   @Override
   String getTestInput() {
-    return "";
+    return "MMMSXXMASM\n" +
+        "MSAMXMSMSA\n" +
+        "AMXSXMAAMM\n" +
+        "MSAMASMSMX\n" +
+        "XMASAMXAMM\n" +
+        "XXAMMXXAMA\n" +
+        "SMSMSASXSS\n" +
+        "SAXAMASAAA\n" +
+        "MAMMMXMMMM\n" +
+        "MXMXAXMASX";
   }
 
   @Override
   String getRealInput() {
-    return "";
+    return "MMMSXXMASM\n" +
+        "MSAMXMSMSA\n" +
+        "AMXSXMAAMM\n" +
+        "MSAMASMSMX\n" +
+        "XMASAMXAMM\n" +
+        "XXAMMXXAMA\n" +
+        "SMSMSASXSS\n" +
+        "SAXAMASAAA\n" +
+        "MAMMMXMMMM\n" +
+        "MXMXAXMASX";
   }
 }
