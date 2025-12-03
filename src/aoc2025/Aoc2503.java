a6fd8114b57d2e08406d63e4a2928fc2d0761c6d
Ihor Trypolskyi <itrypolskyi@hubspot.com>
2025-12-03 15:57:24 +0100

    2503

--- src/aoc2025/Aoc2503.java
+++ src/aoc2025/Aoc2503.java
@@ -2,11 +2,55 @@ package aoc2025;
 
 public class Aoc2503 extends AbstractAoc {
   long part1(String[] in) {
-    return 0;
+    long rr = 0;
+    for (String s : in) {
+      int resultLength = 2;
+      long numberBuilder = 0;
+      int left = 0;
+      while (resultLength-- > 0) {
+        int right = s.length() - resultLength;
+        int maxVal = 0;
+        int maxPos = -1;
+        //search for max value in applicable range
+        for (int i = left; i < right; i++) {
+          int val = Character.getNumericValue(s.charAt(i));
+          if (val > maxVal) {
+            maxVal = val;
+            maxPos = i;
+          }
+        }
+        numberBuilder = numberBuilder * 10 + maxVal;
+        left = maxPos + 1;
+      }
+      rr += numberBuilder;
+    }
+    return rr;
   }
 
   long part2(String[] in) {
-    return 0;
+    long rr = 0;
+    for (String s : in) {
+      int resultLength = 12;
+      long numberBuilder = 0;
+      int left = 0;
+      while (resultLength-- > 0) {
+        int right = s.length() - resultLength;
+        int maxVal = 0;
+        int maxPos = -1;
+        //search for max value in applicable range
+        for (int i = left; i < right; i++) {
+          int val = Character.getNumericValue(s.charAt(i));
+          if (val > maxVal) {
+            maxVal = val;
+            maxPos = i;
+          }
+        }
+        numberBuilder = numberBuilder * 10 + maxVal;
+        left = maxPos + 1;
+      }
+      rr += numberBuilder;
+    }
+    return rr;
   }
 
   public static void main(String[] args) {
@@ -15,21 +59,27 @@ public class Aoc2503 extends AbstractAoc {
 
   @Override
   String getTestInput() {
-    return "";
+    return "987654321111111\n" +
+        "811111111111119\n" +
+        "234234234234278\n" +
+        "818181911112111";
   }
 
   @Override
   String getRealInput() {
-    return "";
+    return "987654321111111\n" +
+        "811111111111119\n" +
+        "234234234234278\n" +
+        "818181911112111";
   }
 
   @Override
   long getTestExpected1() {
-    return 0;
+    return 357;
   }
 
   @Override
   long getTestExpected2() {
-    return 0;
+    return 3121910778619L;
   }
 }
---
+++ src/aoc2025/Aoc2505.java
@@ -0,0 +1,35 @@
+package aoc2025;
+
+public class Aoc2505 extends AbstractAoc {
+  long part1(String[] in) {
+    return 0;
+  }
+
+  long part2(String[] in) {
+    return 0;
+  }
+
+  public static void main(String[] args) {
+    (new Aoc2505()).start();
+  }
+
+  @Override
+  String getTestInput() {
+    return "";
+  }
+
+  @Override
+  String getRealInput() {
+    return "";
+  }
+
+  @Override
+  long getTestExpected1() {
+    return 0;
+  }
+
+  @Override
+  long getTestExpected2() {
+    return 0;
+  }
+}
